bcf610f9d5d9e5cad27806a9fe6ea381de5ec143
yoelvp <yoelvp73@gmail.com>
2023-12-08 01:14:21 -0500

    modify: add rehypeClasses for markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,28 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import addClasses from 'rehype-add-classes'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  markdown: {
+    rehypePlugins: [
+      [
+        addClasses,
+        {
+          h1: 'text-4xl font-bold white-to-primary',
+          h2: 'text-2xl font-bold white-to-primary',
+          h3: 'text-xl font-bold white-to-primary',
+          h4: 'text-lg font-bold white-to-primary',
+          h5: 'font-bold white-to-primary',
+          h6: 'font-bold white-to-primary',
+          img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
+          p: 'mb-6 white-to-primary',
+          a: 'underline underline-offset-2 hover:text-primary decoration-primary white-to-primary',
+          ul: 'list-disc'
+        }
+      ]
+    ]
+  }
 });
