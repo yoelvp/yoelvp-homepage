7fb68cb42b5efb48bdb66ffc96b69caaa890d3aa
yoelvp <yoelvp73@gmail.com>
2024-04-10 02:53:24 -0500

    fix: change color in selection

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,14 +12,14 @@ export default defineConfig({
       [
         addClasses,
         {
-          h1: 'text-4xl font-bold white-to-primary',
-          h2: 'text-2xl font-bold white-to-primary',
-          h3: 'text-xl font-bold white-to-primary',
-          h4: 'text-lg font-bold white-to-primary',
-          h5: 'font-bold white-to-primary',
-          h6: 'font-bold white-to-primary',
-          img: 'border border-slate-300 dark:border-zinc-700 rounded-xl mb-6',
-          p: 'mb-6 white-to-primary',
+          h1: 'text-zinc-800 dark:text-zinc-200 text-4xl font-semibold white-to-secondary dark:white-to-primary',
+          h2: 'text-zinc-800 dark:text-zinc-200 text-2xl font-semibold white-to-secondary dark:white-to-primary',
+          h3: 'text-zinc-800 dark:text-zinc-200 text-xl font-semibold white-to-secondary dark:white-to-primary',
+          h4: 'text-zinc-800 dark:text-zinc-200 text-lg font-semibold white-to-secondary dark:white-to-primary',
+          h5: 'text-zinc-800 dark:text-zinc-200 font-semibold white-to-secondary dark:white-to-primary',
+          h6: 'text-zinc-800 dark:text-zinc-200 font-semibold white-to-secondary dark:white-to-primary',
+          img: 'border border-slate-200 dark:border-zinc-700 rounded-xl mb-6',
+          p: 'mb-6 white-to-secondary dark:white-to-primary',
           a: 'underline underline-offset-2 hover:text-primary decoration-primary white-to-primary',
           ul: 'list-disc'
         }
