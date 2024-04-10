1e98e01bc8b6ac73b560d0c98969b04a6e7de249
yoelvp <yoelvp73@gmail.com>
2024-04-10 02:52:27 -0500

    fix: change color in light mode
    Change color when selecting text and text color in light mode

--- src/components/CategoryMenu.tsx
+++ src/components/CategoryMenu.tsx
@@ -9,7 +9,7 @@ export default function CategoryMenu() {
       className="relative inline-block text-left"
       aria-label='menu'
     >
-      <Menu.Button className="inline-flex justify-center items-center gap-2 rounded-md border border-zinc-400 dark:border-zinc-400 p-2 text-sm font-bold shadow-sm hover:bg-orange-200 dark:hover:bg-zinc-800 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:ring-offset-gray-100 transition-all duration-300">
+      <Menu.Button className="inline-flex justify-center items-center gap-2 rounded-md border border-zinc-400 dark:border-zinc-400 p-2 text-sm font-bold shadow-sm hover:bg-orange-200 dark:hover:bg-zinc-800 focus:outline-none focus:ring-2 focus:ring-secondary dark:focus:ring-primary focus:ring-offset-2 focus:ring-offset-gray-100 transition-all duration-300">
         Categorias
         <BiSolidCategory />
       </Menu.Button>
@@ -31,6 +31,7 @@ export default function CategoryMenu() {
                 className={`
                   ${active && 'bg-orange-200 dark:bg-zinc-700'}
                   block px-4 py-2 text-sm
+                  white-to-secondary dark:white-to-primary
                 `}
               >
                 Todos
@@ -44,6 +45,7 @@ export default function CategoryMenu() {
                 className={`
                   ${active && 'bg-orange-200 dark:bg-zinc-700'}
                   block px-4 py-2 text-sm
+                  white-to-secondary dark:white-to-primary
                 `}
               >
                 Personal
@@ -57,6 +59,7 @@ export default function CategoryMenu() {
                 className={`
                   ${active && 'bg-orange-200 dark:bg-zinc-700'}
                   block px-4 py-2 text-sm
+                  white-to-secondary dark:white-to-primary
                 `}
               >
                 Colaboraciones
