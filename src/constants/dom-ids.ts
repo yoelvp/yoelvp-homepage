92f2ae3f0bbd56c68b00b59b894e3000a6888949
yoelvp <yoelvp73@gmail.com>
2024-05-16 17:04:57 -0500

    feat: add ids for dom

---
+++ src/constants/dom-ids.ts
@@ -0,0 +1,7 @@
+export const DomIds = {
+  MOBILE_MENU_CONTENT_ID: 'mobileMenuContentId',
+  HAMBURGER_BUTTON: 'hamburgerButton',
+  HAMBURGER_OPEN_MENU_BUTTON: 'hamburgerOpenMenuButton',
+  HAMBURGER_CLOSE_MENU_BUTTON: 'hamburgerCloseMenuButton',
+  DOWNLOAD_CV_BUTTON: 'downloadCvButton'
+}
