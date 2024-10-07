ab0c384d10bf41496fa34dd6c3e77728b27ad309
yoelvp <yoelvp73@gmail.com>
2024-10-07 02:10:42 -0500

    feat: add pages

---
+++ src/constants/pages.ts
@@ -0,0 +1,20 @@
+import type { Page } from "@/types/Page";
+
+export const pages: Page[] = [
+  {
+    name: 'Inicio',
+    href: '/'
+  },
+  {
+    name: 'Proyectos',
+    href: '/projects'
+  },
+  {
+    name: 'Acerca',
+    href: '/about'
+  },
+  {
+    name: 'Posts',
+    href: '/posts'
+  }
+]
