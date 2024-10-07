8ff1801487642ae07df21b5d96228dc4a7ea8bdc
yoelvp <yoelvp73@gmail.com>
2024-10-06 22:56:47 -0500

    fix: change config for dev

--- astro.config.ts
+++ astro.config.ts
@@ -2,5 +2,16 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yoelvalverde.dev'
+  site: 'https://yoelvalverde.dev',
+  compressHTML: true,
+  devToolbar: {
+    enabled: false
+  },
+  vite: {
+    server: {
+      watch: {
+        usePolling: true
+      }
+    }
+  }
 })
