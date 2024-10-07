56ab17b8f1f1488064864693b60e347a00a82df9
yoelvp <yoelvp73@gmail.com>
2024-10-06 22:06:19 -0500

    fix: add typing to env

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
+// import '../.astro/types.d.ts'
+
+interface ImportMetaEnv {
+  readonly PUBLIC_R2_BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
