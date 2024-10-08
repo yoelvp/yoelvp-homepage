6e6b8763d91a3f4131219b1f47c4dcccd6423103
yoelvp <yoelvp73@gmail.com>
2024-10-08 01:20:04 -0500

    feat: add experience type

---
+++ src/types/Experience.ts
@@ -0,0 +1,13 @@
+export interface Experience {
+  startDate: string
+  endDate: string
+  position: string
+  companyName: string
+  companyWebsite: string
+  details: string[]
+  projects?: Array<{
+    name: string
+    link: string
+  }>
+  stack: string[]
+}
