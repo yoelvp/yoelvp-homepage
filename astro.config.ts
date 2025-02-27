ac334bf5069e5815732b73a327fee419eb862c0a
yoelvp <yoelvp73@gmail.com>
2025-02-27 12:11:57 -0500

    fix: update config

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yoelvalverde.dev',
+  site: "https://yoelvalverde.dev",
   compressHTML: true,
   devToolbar: {
     enabled: false
